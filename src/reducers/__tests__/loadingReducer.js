f3badbcb519188bdfb54b9d8ca2104835b7c101a
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-21 16:26:50 -0700

    Add loadingReducer and tests for the reducer

---
+++ src/reducers/__tests__/loadingReducer.js
@@ -0,0 +1,22 @@
+import loadingReducer from '../loadingReducer';
+
+describe('loadingReducer', () => {
+  it('should should return the initial state', () => {
+    //setup
+    const expected = false;
+    //execution
+    const result = loadingReducer(undefined, {})
+    //expectation
+    expect(result).toEqual(expected)
+  })
+
+  it('should return the action.isLoading value', () => {
+    //setup
+    const mockAction = {type: 'SET_LOADING', isLoading: true}
+    const expected = true
+    //execution
+    const result = loadingReducer(undefined, mockAction);
+    //expectation
+    expect(result).toEqual(expected)
+  })
+})
