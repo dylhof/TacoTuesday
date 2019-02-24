086eaa5ce040b720650660569d20d653e204e0f0
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-24 11:42:48 -0700

    Add more information and Home button to the 404 error

--- src/components/ErrorDisplay/ErrorDisplay.js
+++ src/components/ErrorDisplay/ErrorDisplay.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import {Typography, Button} from '@material-ui/core';
 
-export const ErrorDisplay = () => {
+export const ErrorDisplay = (props) => {
   return(
-    <h1> 404 not found</h1>
+    <div>
+      <Typography variant='display3'> 404 Taco not found</Typography>
+      <Typography variant='display2'> The path name http://localhost:/3000{props.location.pathname} does not contain any tacos</Typography>
+      <Link to='/'>
+      <Button>Return To Tacos</Button>
+      </Link>
+    </div>  
   )
 }
+
+export default withRouter(ErrorDisplay)
+
