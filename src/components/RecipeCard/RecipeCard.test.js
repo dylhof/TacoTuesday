ff91d7fdbdeec08cd1c2fece27f5ea4a2ec3670e
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-24 12:40:37 -0700

    Add snapshot test for Recipe card

---
+++ src/components/RecipeCard/RecipeCard.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {RecipeCard} from './RecipeCard';
+
+describe('RecipeCard', () => {
+  let wrapper;
+  let mockRecipe = {
+    slug: 'Yum_Taco', 
+    name: 'yummyTacos',
+    recipe: 'lots of steps \n another step'}
+  let mockClasses = {card: {}, list: {}}
+
+  it('should match the snapshot', () => {
+    //setup
+    wrapper = shallow(
+      <RecipeCard 
+      tacoRecipe={mockRecipe}
+      classes={mockClasses}/>)
+    //expectation
+    expect(wrapper).toMatchSnapshot()
+  })
+})
