96a725de2f7d3f126dcd7115900463993d744fc2
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-25 09:49:08 -0700

    Finish out action tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 export const SET_LOADING = 'SET_LOADING';
 export const SET_RANDO_TACO = 'SET_RANDO_TACO';
 export const SET_ERROR = 'SET_ERROR';
+export const SET_BASE_LAYERS = 'SET_BASE_LAYERS';
+export const SET_MIXINS = 'SET_MIXINS';
+export const SET_SEASONINGS = 'SET_SEASONINGS';
+export const SET_CONDIMENTS = 'SET_CONDIMENTS';
+export const SET_SHELLS = 'SET_SHELLS';
+
 
 export const setLoading = isLoading => ({
   type: SET_LOADING,
@@ -18,26 +24,26 @@ export const setError = error => ({
 })
 
 export const setBaseLayers = baseLayers => ({
-  type: 'SET_BASE_LAYERS',
+  type: SET_BASE_LAYERS,
   baseLayers
 })
 
 export const setMixins = mixins => ({
-  type: 'SET_MIXINS',
+  type: SET_MIXINS,
   mixins
 })
 
 export const setSeasonings = seasonings => ({
-  type: 'SET_SEASONINGS',
+  type: SET_SEASONINGS,
   seasonings
 })
 
 export const setCondiments = condiments => ({
-  type: 'SET_CONDIMENTS',
+  type: SET_CONDIMENTS,
   condiments
 })
 
 export const setShells = shells => ({
-  type: 'SET_SHELLS',
+  type: SET_SHELLS,
   shells
 })
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,32 +1,82 @@
 import * as actions from './index'
 
 describe('actions', () => {
+  let mockTacoPart
+  beforeEach(() => {
+    mockTacoPart = [{name:'yumm taco'}]
+  });
+
   it('should have a type of SET_LOADING and a bool', () => {
     //setup
-    const expectedAction = { type: 'SET_LOADING', isLoading: true}
+    const expectedAction = { type: actions.SET_LOADING, isLoading: true}
     //execution
     const result = actions.setLoading(true)
     //expectation
     expect(result).toEqual(expectedAction)
-  })
+  });
 
   it('should have a type of SET_RANDO_TACO and a taco', () => {
     //setup 
     const mockTaco = {baseLayer: 'yummmm'}
-    const expectedAction = { type: 'SET_RANDO_TACO', taco: mockTaco}
+    const expectedAction = { type: actions.SET_RANDO_TACO, taco: mockTaco}
     //execution
     const result = actions.setRandoTaco(mockTaco)
     //expectation
     expect(result).toEqual(expectedAction)
-  })
+  });
 
   it('should have a SET_ERROR and an error message', () => {
     //setup
     const mockError = 'Why are all the tacos gone!?'
-    const expectedAction = { type: 'SET_ERROR', error: mockError }
+    const expectedAction = { type: actions.SET_ERROR, error: mockError }
     //execution
     const result = actions.setError(mockError)
     //expectation
     expect(result).toEqual(expectedAction);
+  });
+
+  it('should have a SET_BASE_LAYERS type and a baselayer array', () => {
+    //setup
+    const expectedAction = { type: actions.SET_BASE_LAYERS, baseLayers: mockTacoPart}
+    //execution
+    const result = actions.setBaseLayers(mockTacoPart)
+    //expectation
+    expect(result).toEqual(expectedAction)
+  });
+
+  it('should have a SET_MIXINS type and a mixins array', () => {
+    //setup
+    const expectedAction = { type: actions.SET_MIXINS, mixins: mockTacoPart}
+    //execution
+    const result = actions.setMixins(mockTacoPart)
+    //expectation
+    expect(result).toEqual(expectedAction);
+  });
+
+  it('should have a SET_SEASONINGS type and a seasonings array', () => {
+    //setup
+    const expectedAction = { type: actions.SET_SEASONINGS, seasonings: mockTacoPart}
+    //execution
+    const result = actions.setSeasonings(mockTacoPart)
+    //expectation
+    expect(result).toEqual(expectedAction);
+  });
+
+  it('should have a SET_CONDIMENTS type and a condiments array', () => {
+    //setup
+    const expectedAction = {type: actions.SET_CONDIMENTS, condiments: mockTacoPart}
+    //execution
+    const result = actions.setCondiments(mockTacoPart)
+    //expectation
+    expect(result).toEqual(expectedAction)
+  });
+
+  it('should have a SET_SHELLS type and a shells array', () => {
+    //setup
+    const expectedAction = {type: actions.SET_SHELLS, shells: mockTacoPart}
+    //execution
+    const result = actions.setShells(mockTacoPart)
+    //expectation
+    expect(result).toEqual(expectedAction)
   })
-})
+});
