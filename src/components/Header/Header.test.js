e334532fa55bb2c7d25e5767cf3a617d0b29cad7
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-24 22:03:40 -0700

    Add Header test for the toggle show menu function

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -3,8 +3,18 @@ import { shallow } from 'enzyme';
 import { Header } from './Header';
 
 describe('Header', () => {
-  const wrapper = shallow(<Header/>);
+  let wrapper;
+  beforeEach(() =>{
+    wrapper = shallow(<Header/>);
+  });
+
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should toggle the showMenu property in state', () => {
+    expect(wrapper.state('showMenu')).toEqual(false);
+    wrapper.instance().toggleShowMenu();
+    expect(wrapper.state('showMenu')).toEqual(true);
+  })
 });
