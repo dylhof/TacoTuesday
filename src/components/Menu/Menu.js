42dba1429d73eb88e95cca38b6e03caa8834a8e7
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-24 20:38:33 -0700

    Remove all MUI except Drawer

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import {Drawer, List, ListItem, ListSubheader, Typography} from '@material-ui/core';
+import { Drawer } from '@material-ui/core';
 import { Link } from 'react-router-dom'
 
 const Menu = (props) => {
-  console.log(props.showMenu)
   return (
-    <Drawer open={props.showMenu} onClose={props.toggleShowMenu}>
+    <Drawer 
+    open={props.showMenu} 
+    onClose={props.toggleShowMenu}>
       <div
-      role="button"
-      onClick={props.toggleShowMenu}
-      onKeyDown={props.toggleShowMenu}>
-
-          <Link to='/'><Typography variant='display2'>Taco Home</Typography></Link>
-          <List>
-            <ListSubheader>Explore</ListSubheader>
-            <ListItem><Link to='/explore/baseLayers'>Base-Layers</Link></ListItem>
-            <ListItem><Link to='/explore/mixins'>Mixins</Link></ListItem>
-            <ListItem><Link to='/explore/condiments'>Condiments</Link></ListItem>
-            <ListItem><Link to='/explore/seasonings'>Seasonings</Link></ListItem>
-            <ListItem><Link to='/explore/shells'>Shells</Link></ListItem>
-          </List>
-          <Link to='/build_a_taco'><Typography variant='display2'>Build A Taco</Typography></Link>
+        role="button"
+        onClick={props.toggleShowMenu}
+        onKeyDown={props.toggleShowMenu}>
+        <Link to='/'>
+          <h3 variant='display2'>
+            Taco Home
+          </h3>
+        </Link>
+        <ul>
+          <h3>Explore</h3>
+          <li>
+            <Link to='/explore/baseLayers'>
+              Base-Layers
+            </Link>
+          </li>
+          <li>
+            <Link to='/explore/mixins'>
+              Mixins
+            </Link>
+          </li>
+          <li>
+            <Link to='/explore/condiments'>
+              Condiments
+            </Link>
+          </li>
+          <li>
+            <Link to='/explore/seasonings'>
+              Seasonings
+            </Link>
+          </li>
+          <li>
+            <Link to='/explore/shells'>
+              Shells
+            </Link>
+          </li>
+        </ul>
+        <Link to='/build_a_taco'>
+          <h3 variant='display2'>
+            Build A Taco
+          </h3>
+        </Link>
       </div>
     </Drawer>
   )
