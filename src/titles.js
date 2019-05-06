a0877d82d8b296482fbad84e21968b46adfdc981
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-05-06 11:09:53 -0600

    Add title objects

---
+++ src/titles.js
@@ -0,0 +1,10 @@
+
+export const home = {
+    title: 'home',
+    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+  }
+
+  export const mixins = {
+    title: 'mixins',
+    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+  }
