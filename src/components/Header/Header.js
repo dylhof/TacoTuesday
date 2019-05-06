b9cf798296c679488da57ba3a216a73d8c1231aa
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-05-06 11:11:02 -0600

    Remove Title from Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import  Menu  from '../Menu/Menu';
-import Title from '../../containers/Title/Title';
-
 
 export class Header extends Component {
   state = {
@@ -18,7 +16,6 @@ export class Header extends Component {
       {!this.state.showMenu && 
          <i className="fas fa-chevron-right fa-2x" onClick={this.toggleShowMenu}></i>}
         <h1>The Taste Of Taco Tuesday</h1>
-        <Title/>
         {this.state.showMenu && <Menu toggleShowMenu={this.toggleShowMenu} showMenu={this.state.showMenu}/>}
       </header>
     )
