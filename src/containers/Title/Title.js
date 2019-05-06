dc421afc3f2f5f56ee3f049dc4c70b1e705e67b0
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-05-06 11:13:26 -0600

    Add functionality to dynamically show a title and description based on pathname

--- src/containers/Title/Title.js
+++ src/containers/Title/Title.js
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import * as title from '../../titles';
 
 export class Title extends Component{
-  
+  constructor(props) {
+    super(props)
+  }
+
+  findTitle = () => {
+    switch (this.props.location.pathname) {
+      case '/':
+        return 'home'
+      case '/explore/mixins':
+          return 'mixins'
+      default:
+        return 'TACOS!'
+    }
+  }
+
   render(){
+    const page = this.findTitle()
     return(
-      <p>this is the home page</p>
+      <div>
+          <h3>{title[page].title}</h3>
+          <p>{title[page].description}</p>
+      </div>
     )
   }
 }
 
-export default Title;
+export default withRouter(Title);
