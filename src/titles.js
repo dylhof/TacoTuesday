cf3dba6ee039a224e0612f1b0783f5d236eebd3d
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-05-08 17:16:43 -0600

    Add titles for various pathways

--- src/titles.js
+++ src/titles.js
@@ -5,6 +5,30 @@ export const home = {
   }
 
   export const mixins = {
-    title: 'mixins',
+    title: 'Mixins',
+    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+  }
+
+  export const condiments = {
+    title: 'Condiments',
+    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+  }
+
+  export const shells = {
+    title: 'Shells',
+    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+  }
+
+  export const baselayers = {
+    title: 'Base Layers',
+    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+  }
+  export const seasonings = {
+    title: 'Seasonings',
+    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+  }
+
+  export const build_a_taco = {
+    title: 'Build A Taco',
     description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
   }
