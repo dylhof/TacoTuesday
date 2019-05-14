e135958e88fe5ba45bb469ae2abfd33c75ba3a35
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-05-14 12:35:40 -0600

    Add custom descriptions to taco parts that need titles

--- src/titles.js
+++ src/titles.js
@@ -1,31 +1,31 @@
 
 export const home = {
-    title: 'home',
+    title: 'Home',
     description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
   }
 
   export const mixins = {
     title: 'Mixins',
-    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+    description: 'Ahhh... Mixins! The beautiful fresh crunch below the condiments and above the rich baselayer. From the simplicity of lettuce to the complexity of Mushroom Wasabi Salad, browse and find your perfect mixin.'
   }
 
   export const condiments = {
     title: 'Condiments',
-    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+    description: 'All up at the tippy top of your terrific taco, take in the tasty tid-bits that are the condiments.  Salsas and sauces, cheeses and creams, take a look for the condiment that will top off your perfect taco! '
   }
 
   export const shells = {
     title: 'Shells',
-    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+    description: 'The edible wrapper that keeps the taco together.  Without the shell there is no taco.  The taco is the shell.  Choose wisely. (Although all of them are really pretty good so try them all!)'
   }
 
   export const baselayers = {
     title: 'Base Layers',
-    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+    description: 'The Base Layer.  The foundation of a taco. The start. The End. The main event.  Choose wisely as the base-layer will be the first thing in and the last thing you taste.  Welcome to the base-layer of awesome!'
   }
   export const seasonings = {
     title: 'Seasonings',
-    description: 'Tired of the same boring taco being served Tuesday after Tuesday? You’ve come to the right place!  Find all the recipes to make your Taco Tuesday exciting, delicious and full fledged Teriffic Taco Tuesday!'
+    description: 'The sensational scents of the seasonings bring a taco to life and set the tone for your taco\'s taste profile.'
   }
 
   export const build_a_taco = {
